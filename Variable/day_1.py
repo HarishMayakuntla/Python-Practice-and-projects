a5a43790799279dddd82cd281097811938f493e5
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-06-30 15:09:02 +0530

    updated code

---
+++ Variable/day_1.py
@@ -0,0 +1,31 @@
+# Variables can change
+x = 10
+print(x)
+x = 20
+print(x)
+
+# Multiple variables in one line
+a, b, c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+# Same value to multiple variables
+i = j = l = 100
+print(i, j, l)
+
+# Checking type of variable
+x = 10
+print(type(x))
+
+# Dynamic typing(Python automatically changes type)
+s = 10
+s = "Hello"
+s = 3.5
+
+'''## A variable is:
+
+1.a container for data
+2.can change value
+3.can store different types
+4.used in every Python program '''
