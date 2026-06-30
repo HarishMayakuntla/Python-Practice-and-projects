2329d4da1f6a1ca3b9c60938fac57ffdc7a28652
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-06-30 15:33:38 +0530

    updated code

---
+++ Variable/1_Student_Information.py
@@ -0,0 +1,10 @@
+# Student Information System
+name = "Harish"
+age = 21
+course = "Data Science with ml,ai,agentic ai "
+college = "iits College"
+
+print(name)
+print(age)
+print(course)
+print(college)
