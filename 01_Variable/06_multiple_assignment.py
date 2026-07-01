a4259320c86c2be56383c1495c33df6fe8203cf9
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-01 15:15:43 +0530

    Added 02_Operators folder

---
+++ 01_Variable/06_multiple_assignment.py
@@ -0,0 +1,15 @@
+# Multiple assignment
+
+a, b, c = 10, 20, 30
+
+print("a =", a)            #  here we assain 
+print("b =", b)            # 3 variables names = 3 variable values
+print("c =", c)            # separate by ,
+
+# One value assigned to multiple variables
+
+x = y = z = 100
+
+print("x =", x)         # 3 different variables assaining eachother
+print("y =", y)         # and single variable value s
+print("z =", z)
---
+++ 01_Variable/_circle_calculator_mini_project.py
@@ -0,0 +1,12 @@
+radius = 7      
+pi = 3.14159
+
+diameter = 2 * radius
+circumference = 2 * pi * radius
+area = pi * radius * radius
+
+print("----- Circle -----")
+print("Radius        :", radius)
+print("Diameter      :", diameter)
+print("Circumference :", circumference)
+print("Area          :", area)
