1b8283b2f47bb3496c58ba68d3fd59b9a1b8f859
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-01 08:55:19 +0530

    Added new code

---
+++ Variable/_employee_details.py
@@ -0,0 +1,12 @@
+employee_id = 1001
+employee_name = "harish"
+department = "IT"
+salary = 30000
+experience = 'fresher'
+
+print("----- Employee Details -----")
+print("Employee ID :", employee_id)
+print("Name        :", employee_name)
+print("Department  :", department)
+print("Salary      :", salary)
+print("Experience  :", experience)
