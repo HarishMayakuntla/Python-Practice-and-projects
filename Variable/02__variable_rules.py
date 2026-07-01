42149157d257d206f2dc77afb3bb6048bceb8cb9
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-01 08:27:30 +0530

    Added new code

---
+++ Variable/02__variable_rules.py
@@ -0,0 +1,16 @@
+# Valid variable names
+
+student_name = "Harish"
+_age = 21
+marks1 = 95
+
+print(student_name)
+print(_age)
+print(marks1)
+
+# Invalid variable names 
+#  ther are :
+# 1name = "Harish"
+# student-name = "Harish"
+# @name = "Harish"
+#    Shows   error  invalid variable
