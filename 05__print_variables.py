a4d8fbc1f4e52b90598bc5f76fc7e4808bb6ee3e
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-01 08:39:51 +0530

    Added new code

---
+++ 05__print_variables.py
@@ -0,0 +1,19 @@
+name = "Harish"
+age = 21
+city = "Hyderabad"
+
+print(name)
+print(age)
+print(city)
+
+print("----------------")
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+print("----------------")
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I live in {city}.")
