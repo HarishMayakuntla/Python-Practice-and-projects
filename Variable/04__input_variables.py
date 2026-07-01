6616a170070509eb04b966a37b9af92d7f60e1fb
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-01 08:35:52 +0530

    Added new code

---
+++ Variable/04__input_variables.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+
+
+## input  means that the computer asks user 
+# int is number
+# flaot  is decimal
