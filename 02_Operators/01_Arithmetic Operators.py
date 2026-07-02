940c6f4807fff1473d8a422f9afbcf4b025781c6
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-02 15:13:15 +0530

    Added operators examples

---
+++ 02_Operators/01_Arithmetic Operators.py
@@ -0,0 +1,14 @@
+# Operators in Python
+# Arithmetic Operators
+# we can perform the basic calculation
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print('Floor Division',a//b)
+print('Modulus (Remainder)',a%b)
+print('power',a**b)
+
