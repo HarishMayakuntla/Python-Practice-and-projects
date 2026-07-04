71f658c2915ebe27feb883f1a5f66f38e5a36ca8
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-04 10:52:37 +0530

    Added comparison operators examples

---
+++ 02_Operators/04_Assignment Operators.py
@@ -0,0 +1,38 @@
+# Assignment Operators Example
+# Used to assign values to variables
+
+
+a = 10
+print("Initial Value of a =", a)
+
+# += (Add and Assign),Adds a value from variable
+a += 5
+print("After a += 5 :", a)   
+
+# -= (Subtract and Assign),Subtracts a value from the variable
+a -= 3
+print("After a -= 3 :", a)   
+
+# *= (Multiply and Assign),multiple a value from variable
+a *= 2
+print("After a *= 2 :", a)
+
+# /= (Divide and Assign),divide a value from variable
+a /= 4
+print("After a /= 4 :", a)
+
+# //= (Floor Divide and Assign),Performs floor division and stores the result
+a //= 2
+print("After a //= 2 :", a)
+
+# %= (Modulus and Assign),stores the remainder after division
+a %= 3
+print("After a %= 3 :", a)
+
+# **= (Exponent and Assign),the number to a power
+a **= 3
+print("After a **= 3 :", a)
+
+# = (Simple Assignment),Assigns a value to 'a' variable
+a = 50
+print("After a = 50 :", a)
