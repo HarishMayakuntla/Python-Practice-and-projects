5231cdcad9b54721f52319ac7fe56f33dd76b238
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-04 10:38:20 +0530

    Added comparison operators examples

---
+++ 02_Operators/03.Comparison Operators .py
@@ -0,0 +1,16 @@
+# Comparison Operators Example
+
+a = 10
+b = 20
+
+print("Value of a =", a)
+print("Value of b =", b)
+
+print("Comparison Operators")
+
+print("a == b :", a == b)   # Equal to
+print("a != b :", a != b)   # Not Equal to
+print("a > b  :", a > b)    # Greater Than
+print("a < b  :", a < b)    # Less Than
+print("a >= b :", a >= b)   # Greater Than or Equal to
+print("a <= b :", a <= b)   # Less Than or Equal to
