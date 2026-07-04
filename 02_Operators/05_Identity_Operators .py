632883d3c4e492eb6a76e3e00457e893373987af
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-04 11:22:43 +0530

    Added comparison operators examples

--- 02_Operators/05_Identity_Operators .py
+++ 02_Operators/05_Identity_Operators .py
@@ -12,3 +12,4 @@ print("a is b     :", a is b)
 print("a is c     :", a is c)
 print("a is not c :", a is not c)
 print("b is not c :", b is not c)
+
---
+++ 02_Operators/06_Membership_Operators.py
@@ -0,0 +1,13 @@
+# Membership Operators  
+# -Check if a value exists in a sequence.
+
+name = 'harish mayakuntla'
+print('h' in name)    # here use 'in'  ,'h' letter exits the given string,so statement is True
+print('z' in name)    # here use 'in'  ,'z' letter does not exits the given string ,so statement is False
+
+print('h' not in name) # here use ' not in '  ,'h' letter exits the given string , so statement is False
+print('z' not in name) # here use ' not in '  ,'z' letter does not exits the given string ,so statement is True
+
+number=20,50,30
+print(50 in number)      # here use 'in'  , 50  number exits the given string , so statement is True
+print(50 not in number)  # here use ' not in '  , 50  number exits the given string , so statement is False
