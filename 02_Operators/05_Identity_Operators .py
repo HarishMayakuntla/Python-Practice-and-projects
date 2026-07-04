4f63a51952684780488e988e9708bc2250c15ab8
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-04 10:58:25 +0530

    Added comparison operators examples

---
+++ 02_Operators/05_Identity_Operators .py
@@ -0,0 +1,14 @@
+# Identity Operators Example
+
+a = 20
+b = a
+c = 30
+
+print("a:", a)
+print("b:", b)
+print("c:", c)
+
+print("a is b     :", a is b)
+print("a is c     :", a is c)
+print("a is not c :", a is not c)
+print("b is not c :", b is not c)
