0efee3814f438ae863e1ac2aacf033c4a517fdf1
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:29:28 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/07.f_string.py
@@ -0,0 +1,10 @@
+name = "Harish"
+age = 22
+
+print(f"My name is {name} and I am {age} years old.")
+
+
+
+#  here we use f''  string or formatted stringor string formatting.
+# starts with f '' and between quotes we write statements
+# and we can call objects in between{}
