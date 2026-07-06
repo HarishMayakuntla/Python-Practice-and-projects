47efba6fdf7e2ed42c27dfbef206ca204888f27f
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:10:45 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/04_Input_Float.py
@@ -0,0 +1,13 @@
+# here input() take defalut as string 
+# then we need change the occurs of user need
+#  example . i need num but input take string what wii happen
+            # it cannot perform mathematical calculations
+# at the time we use type converstions like int(input()) ,flaot(iput()) etc
+
+
+user=float(input('enter a name : ')) # it take only numbers
+print(user)
+
+
+# here the output will come decimals like 2.14 
+# float value give int to decimal values S
