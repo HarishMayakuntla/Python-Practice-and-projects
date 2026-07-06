3c61028d73e02e8d56af086c8deb0d90be516bd4
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:34:14 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/09_separator.py
@@ -0,0 +1,9 @@
+#  separator (sep)
+
+print(10, 20, 30, sep="-") # it will separate by '-'
+                           # output= 10-20-30
+
+
+# here the sep='-' keyword
+# it is separate the value to '-'
+# like output= 10-20-30
