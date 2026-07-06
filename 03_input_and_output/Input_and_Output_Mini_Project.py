9776b6c98571e42122a3a017478dbdb981ab3e83
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:39:34 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/Input_and_Output_Mini_Project.py
@@ -0,0 +1,14 @@
+print("===== Student Information =====")
+
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+course = input("Enter Course: ")
+marks = float(input("Enter Marks: "))
+
+print("\n===== Student Details =====")
+print(f"Name   :, {name}")
+print("Age    :", age)
+print("Course :", course )
+print("Marks  :", marks)
+
+print(f'{50==marks},pass')
