a4b898aafc343534b0faf4677f22db4d5b11185f
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:22:28 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/06_multiple_integer_inputs.py
@@ -0,0 +1,10 @@
+a, b = map(int, input("Enter two numbers: ").split())
+
+print("Sum =", a + b)
+
+# here take multiple  integer inputs 
+#  multiple values only contain numbers
+# here map()  transforms the data 
+# The map() function is ideal for transforming data in a list. \
+# For example, converting a list of strings to integers
+
