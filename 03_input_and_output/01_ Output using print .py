b372c008f8c0960ff481efcaf458204b041590ad
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 10:56:17 +0530

    Added Python input and output practice

--- 03_input_and_output/01_ Output using print .py
+++ 03_input_and_output/01_ Output using print .py
@@ -1,1 +1,14 @@
 print('hello world')
+
+
+
+# the print statament is built_in_fuction
+# it  is also used as a debuger
+# print is output statement displaying data to the user
+# we can write anything in print in between quotes('' or "") 
+# it will give exact what to wrote inside in between quotes in print statement.
+
+
+print('iabdnbwhhqkmhhhqbb   nddfghjvbnghwwhghjbna')
+print("y2iuhb25555wqdw54ew5q5111x5554@@##$$%^&^%$")
+print('!@#$%^&*()<>?.,/]\|{}|')
