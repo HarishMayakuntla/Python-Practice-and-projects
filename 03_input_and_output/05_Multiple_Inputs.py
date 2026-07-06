e114e204907efd8a1a19fb1fe2584369412720ae
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:14:56 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/05_Multiple_Inputs.py
@@ -0,0 +1,11 @@
+a, b = input("Enter two numbers: ").split()
+
+print("First Number:", a)
+print("Second Number:", b)
+
+
+# here take multiple inputs 
+# multiple variables and multiple values 
+# input().split()
+# used to take multiple inputs from the user in a single line 
+# and split them into a list of strings based on a separator (default is whitespace) 
