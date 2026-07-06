849b6afbb9b6dd7b047daeb2dc6d7effc3a713f7
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:05:22 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/02_Input_using_input.py
@@ -0,0 +1,20 @@
+user=input('enter a name : ')
+print(user)
+
+
+# here input()key word is built_in_fuction 
+# same it ask to user to get input
+# input() take input from user and take perform action
+# here input() take defalut as string 
+# then we need change the occurs of user need
+#  example . i need num but input take string what wii happen
+            # it cannot perform mathematical calculations
+# at the time we use type converstions like int(input()) etc
+
+
+user=int(input('enter a name : ')) # it take only numbers
+print(user)
+
+
+user=float('enter a name : ') # it conveerts number to decimals
+print(user)
