352b5b983e847baeb4fd90f568649eff9eee3c55
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-06 11:07:44 +0530

    Added Python input and output practice

---
+++ 03_input_and_output/03_Input_Integer.py
@@ -0,0 +1,12 @@
+# here input() take defalut as string 
+# then we need change the occurs of user need
+#  example . i need num but input take string what wii happen
+            # it cannot perform mathematical calculations
+# at the time we use type converstions like int(input()) etc
+
+
+user=int(input('enter a name : ')) # it take only numbers
+print(user)
+
+
+# here it is accpts only numbers
