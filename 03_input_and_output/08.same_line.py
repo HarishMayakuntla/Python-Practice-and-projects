0fc2e5bc890063eaa848a852278fe9354f531b90
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-07 10:12:39 +0530

    Added type conversion examples in Python

--- 03_input_and_output/08.same_line.py
+++ 03_input_and_output/08.same_line.py
@@ -1,5 +1,7 @@
 #  here we wrote output in same line
 
 
-print("Python", end=" ") 
+print("Python", end=" ")  # here print same line
 print("Programming")
+
+# end='' is a key word print output in same line
