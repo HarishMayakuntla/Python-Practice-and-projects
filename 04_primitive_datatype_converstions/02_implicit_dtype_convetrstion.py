ac3c5436fadb5ea96bff946ba5e357eb0efabc9f
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-07 10:51:37 +0530

    Added primitive type conversion practice in Python

---
+++ 04_primitive_datatype_converstions/02_implicit_dtype_convetrstion.py
@@ -0,0 +1,11 @@
+# implicit type converstion (automatic)
+# Python automatically converts one data type to another when needed
+
+
+a=25
+b=33.65
+c=a+b # here total 58.65
+print(type(c)) # <class 'float'>
+
+
+# here  we can't use any converstins or casting but it perform automatically.
