cb45d525b2aa5ddec8b8f0f5f9b2dfba9421e6e5
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-08 11:18:33 +0530

    Added conditional statements examples

--- 05_conditional_statements/01_if_stat.py
+++ 05_conditional_statements/01_if_stat.py
@@ -1,4 +1,18 @@
-num = int(input("Enter a number: "))
+# if checks a condition,if it's True go inside  ,False it never execute
+# else runs when the condition is False.
+# elif checks additional conditions.
+# Python uses indentation (4 spaces) to define code blocks.
+# Conditions always evaluate to either True or False.
 
-if num > 0:
+
+num = 6
+if num > 0:   # here check the condition if its True go inside
     print("Positive")
+
+
+num = 7
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
