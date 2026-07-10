905990587ccf5381fb5b224156d2329a0392cd97
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-10 11:25:41 +0530

    Added loop statements examples

--- 06_loop_statements/03_for_control.py
+++ 06_loop_statements/03_for_control.py
@@ -31,4 +31,11 @@ for i in range(10):
 else:
     print('iteration complete')
 
-    
+
+#####  nested loop
+# loop inside loop
+
+for i in range(1,5+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
