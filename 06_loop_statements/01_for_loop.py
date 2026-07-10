5109bee473256e99c58429525b6ed5f10eea4d06
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-10 10:27:06 +0530

    Added loop statements examples

---
+++ 06_loop_statements/01_for_loop.py
@@ -0,0 +1,20 @@
+## for variable in sequence
+
+a=['harish',21,70.13,True]
+for i in a:
+    print(i)  #  it give out put line by line
+
+#output:# harish
+        # 21
+        # 70.13
+        # True
+
+for i in range(1,30):
+    print(i)     # it gives  1  to 29(30-1)  numbers outputs
+
+for i in range(1,30,2):
+    print(i)         # it gives it skip 2 steps and give output
+
+for i in range(len(a)):
+    print(i)            # here we get the indexing of a variable
+
