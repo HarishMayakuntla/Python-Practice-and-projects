96aaa7b7262909c7f53e089e3df56c5e1958fa39
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-10 10:51:09 +0530

    Added loop statements examples

---
+++ 06_loop_statements/04_while_control.py
@@ -0,0 +1,33 @@
+##  infinity loop
+'''
+# while True:
+#     print('hello world')    # it give infinity values
+
+
+## break with while
+#The break statement immediately exits the loop.
+i = 1
+while True:
+    print(i)      # it gives until i bcomes 5 after it exit the loop
+    if i == 5:
+        break
+    i += 1
+
+
+## continue with while
+# The continue statement skips the rest of the current iteration and starts the next iteration.
+i = 0
+
+while i < 5:
+    i += 1               # here i become 5 but it skip 3
+    if i == 3:
+        continue
+    print(i)              '''
+
+## pass with while
+# The pass statement is a placeholder that does nothing.
+i = 1
+
+while i <= 5:
+    pass
+    i += 1
