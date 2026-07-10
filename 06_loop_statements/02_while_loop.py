a4ba11e47213c4e81b7a01e8e5c78b8027456e6c
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-10 10:38:39 +0530

    Added loop statements examples

---
+++ 06_loop_statements/02_while_loop.py
@@ -0,0 +1,24 @@
+## while - it repeat untill condition is True
+
+
+i=2
+while i<=10:
+    print(i)   # here we check the 2 to 10 and increment become 2
+    i+=2
+
+
+count = 5
+while count > 0:    # same here check decrerment
+    print(count)
+    count -= 1
+
+print("Blast Off!")
+
+
+password = ""
+while password != "python":    #  here we check the user input
+    password = input("Enter Password: ") 
+
+print("Access Granted")
+
+
