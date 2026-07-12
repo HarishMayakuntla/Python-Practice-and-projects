b5ca58975a21a02dd6a37f659101656b1d6882d8
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-12 18:35:30 +0530

    Added string practice programs

---
+++ 07_strings/01_basic_string.py
@@ -0,0 +1,19 @@
+
+## -----------single quotes
+
+a=' i am harish  mayakuntla' # it takes as string
+print(a)
+
+##------------ double quotes
+
+a=" i know your a human " # it takes as string
+print(a)
+
+## ----------- Triple quotes  using '''
+
+a=''' a narrator of the  untold story ,he is written by himself ''' # it takes as string
+print(a)
+
+## -------------  Triple quotes  using """
+a= """ you know who am i ?  """ # it takes as string
+print(a)
