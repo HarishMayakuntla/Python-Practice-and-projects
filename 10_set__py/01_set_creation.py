50a4915a9afd95026875b8a54cea7f4130d95a72
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-17 15:17:33 +0530

    Added Python set practice

---
+++ 10_set__py/01_set_creation.py
@@ -0,0 +1,39 @@
+## ================= set creation ===============
+
+## ------------------ single data type--------------------
+
+
+a = {1, 2, 3}          # set contains integer
+
+b={'hari','hi' ,'m',}  # set contains strings like
+
+print(a)   #{1, 2, 3}
+
+print(type(a)) # <class 'set'>
+
+print(b)  # {'hi', 'hari', 'm'}
+
+print(type(b))  # <class 'set'>
+
+
+
+
+## ---------------multiple data tyoes ----------------------
+
+a =  {1, "a", 3.5,True }  # it acces all data types
+
+print(a)   #  {1, "a", 3.5,True } 
+
+print(type(a)) # <class 'set'>
+
+
+## ---------------- reprasentation ----------------
+
+a = {}    # it is not set it is dict
+ 
+print(type(a))  # <class 'dict'>
+
+
+a=set()  # it  showes set
+
+print(type(a)) # <class 'set'>
