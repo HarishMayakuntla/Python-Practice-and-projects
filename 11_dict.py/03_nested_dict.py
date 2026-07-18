d5910bd84cfc5361326bc80165b7b59ff37cc6f1
Harish Mayakuntla <mayakuntlaharish7@gmail.com>
2026-07-18 12:55:04 +0530

    Added dictionary python practice

---
+++ 11_dict.py/03_nested_dict.py
@@ -0,0 +1,25 @@
+## ========== Nested Dictionary in Python ===============
+# A nested dictionary is a dictionary inside another dictionary.
+# syntax :  
+# students = {
+#     key1: {sub_key1: value1, sub_key2: value2},
+#     key2: {sub_key1: value1, sub_key2: value2}
+# }
+
+
+
+students = {
+    101: {
+        "name": "Harish",
+        "age": 22
+    },
+    102: {
+        "name": "Ravi",
+        "age": 21
+    }
+}
+
+print(students[101]["name"])
+
+
+# o/p : 
